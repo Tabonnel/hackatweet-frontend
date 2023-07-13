a1b199b2d19003ae0c759221d903c615c866603b
Thibaud <abonnel.t@gmail.com>
2023-07-13 15:20:46 +0200

    start of login component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Home from '../components/Home';
+import Login from '../components/Login';
 import LastTweet from '../components/LastTweet'
 import Tweet from '../components/Tweet'
 import Trends from '../components/Trends'
 
 function Index() {
-  return <Home />;
+  return <Login />;
 }
 
 export default Index;
