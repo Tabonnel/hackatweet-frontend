4adcfc7180b012416a4ea11fdafa362831e0efa9
Adrien AGARD <agardadrien@gmail.com>
2023-07-14 10:16:16 +0200

    ajout de la fonction des tweets triés et sans boucle infinie

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,9 +1,32 @@
 import styles from "../styles/Tweet.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faXmark, faEye } from "@fortawesome/free-solid-svg-icons";
-import moment from 'moment';
+import { faHeart, faTrash, faEye } from "@fortawesome/free-solid-svg-icons";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function Tweet(props) {
+  let iconStyle = {};
+
+  const handleTrashClick = () => {
+    if (user.username !== props.username) {
+      iconStyle = { display: "none" };
+      return;
+    } else {
+      // fetch(`http://localhost:3000/users/canBookmark/${user.token}`)
+      //   .then((response) => response.json())
+      //   .then((data) => {
+      //     if (data.result && data.canBookmark) {
+      //       if (props.isBookmarked) {
+      //         dispatch(removeBookmark(props));
+      //       } else {
+      //         dispatch(addBookmark(props));
+      //       }
+      //     }
+      //   });
+      iconStyle = { color: "white" };
+    }
+  };
 
   return (
     <div className={styles.tweetContainer}>
@@ -12,15 +35,23 @@ function Tweet(props) {
           className={styles.logoId}
           src="flat-business-man-user-profile-avatar-icon-vector-4333097.jpg"
         />
-        {props.firstname}
-        <p> </p>
-        {props.username}
-        <p>               </p>
+        <p>{props.firstname}</p>
+
+        <p className={styles.username}> {props.username} </p>
+
         {moment(props.date).fromNow()}
       </div>
       <div className={styles.tweetContent}>{props.content}</div>
       <div className={styles.tweetCountlikes}>
-        <FontAwesomeIcon icon={faHeart}> </FontAwesomeIcon>  {props.likeCount}
+        <FontAwesomeIcon icon={faHeart} className={styles.likeHeart}>
+          {" "}
+        </FontAwesomeIcon>
+        {props.likeCount}
+        <FontAwesomeIcon
+          icon={faTrash}
+          className={styles.trash}
+          style={iconStyle}
+        ></FontAwesomeIcon>
       </div>
     </div>
   );
